3e7c2e4ac8d858df27b80f136ef5c81126f4bb0e
sofia <sofiatuleneva@gmail.com>
2019-07-24 13:23:19 +0300

    add Voyager

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Mail;
 
-class User extends Authenticatable
+class User extends \TCG\Voyager\Models\User
 {
     use Notifiable;
 
